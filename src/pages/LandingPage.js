8bfeec562f3703975b1d724939d6a884a3568c74
madeserius <imadeagus0209@gmail.com>
2022-07-19 23:24:31 +0700

    membuat pengulangan ribet bgt sampe dua kondisi

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,15 +3,15 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
-
 import landingPage from "json/landingPage.json";
+import Categories from "parts/Categories";
+
 export default class LandingPage extends Component {
   constructor(props) {
     super(props);
     this.refMostPicked = React.createRef();
   }
   render() {
-    console.log(this.props);
     return (
       <>
         <Header {...this.props}></Header>
@@ -20,6 +20,7 @@ export default class LandingPage extends Component {
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked}
         />
+        <Categories data={landingPage.categories} />
       </>
     );
   }
